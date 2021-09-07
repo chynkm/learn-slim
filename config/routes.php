835e1316eb6694d26321fff971859e58ea1e66ef
Karthik M <chynkm@gmail.com>
2021-09-07 17:36:30 +0530

    Removed all existing files

--- config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Slim\App;
-
-return function (App $app) {
-    $app->get('/', \App\Action\HomeAction::class)->setName('home');
-    $app->post('/users', \App\Action\UserCreateAction::class);
-};
