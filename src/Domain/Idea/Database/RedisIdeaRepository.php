ac66385ad4ae953c39b326d1e5c6b98657b6d982
Karthik M <chynkm@gmail.com>
2021-09-27 22:09:47 +0530

    RedisIdeaRepository

--- src/Domain/Idea/Database/RedisIdeaRepository.php
+++ src/Domain/Idea/Database/RedisIdeaRepository.php
@@ -2,9 +2,12 @@
 
 namespace App\Domain\Idea\Database;
 
-use App\Domain\Idea\Interfaces\IdeaRepository;
+use App\Domain\Idea\IdeaRepository;
 
-class RedisIdeaRepository extends IdeaRepository
+class RedisIdeaRepository implements IdeaRepository
 {
+    public function save(array $idea): void
+    {
 
+    }
 }
